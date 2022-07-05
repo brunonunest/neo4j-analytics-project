44611e57b3570dc2211197811f6fd3359212dc6d
Bruno Trevelin <brunonunest@Brunos-Air.Home>
2022-07-05 20:04:38 +0100

    fixedstringstonumbers

--- src/functions.py
+++ src/functions.py
@@ -13,9 +13,11 @@ def insertFidelityRelClientProfile():
     query = "LOAD CSV WITH HEADERS FROM 'file:///clientsheet.csv' AS line "\
             "MATCH (c:ClientCategory {label: line.Category}) "\
             "MERGE (cp:ClientProfile {clientnum: line.CLIENTNUM}) "\
-            "SET cp.age = line.Age, cp.gender = line.Gender, cp.dependents = line.Dependents, cp.marital_status = line.MaritalStatus "\
+            "WITH toFloat(replace(line.Age, ',', '.')) as age, toFloat(replace(line.Dependents, ',', '.')) as dependents, cp, line "\
+            "SET cp.age = age, cp.gender = line.Gender, cp.dependents = dependents, cp.marital_status = line.MaritalStatus "\
             "MERGE (cp)-[r:FIDELITY]->(c) "\
-            "SET r.products_subscribed = line.ProductsSubscribed, r.inactivity12 = line.Inactivity12months, r.support_requests12 = line.CustomerSupportRequests12months, r.months_as_client = line.MonthsasClient "
+            "WITH toFloat(replace(line.ProductsSubscribed, ',', '.')) as products_subscribed, toFloat(replace(line.Inactivity12months, ',', '.')) as inactivity12, toFloat(replace(line.CustomerSupportRequests12months, ',', '.')) as support_requests12, toFloat(replace(line.MonthsasClient, ',', '.')) as months_as_client "\
+            "SET r.products_subscribed = products_subscribed, r.inactivity12 = inactivity12, r.support_requests12 = support_requests12, r.months_as_client = months_as_client "
     graph.run(query)
 
 def insertIncome():
@@ -35,13 +37,8 @@ def insertEducation():
 def insertCreditCard():
     query = "LOAD CSV WITH HEADERS FROM 'file:///clientsheet.csv' AS line "\
             "MATCH (cp:ClientProfile {clientnum: line.CLIENTNUM}) "\
-            "MERGE (cp)-[r:USES_CARD {utilization_rate: line.CardUtilizationRate}]->(cc:CreditCard {type: line.CardType, limit: line.Limit, used_limit: line.UsedLimit, available_limit: line.AvailableLimit, txs_variation_Q4_Q1: line.TransactionsvaluevariationQ4_Q1, txs12: line.Transactionsvalue12months, number_txs12: line.Transactions12months, number_txs_variation_Q4_Q1: line.VariationTransactionsQ4_Q1}) "
-    graph.run(query)
-
-#set numbers on neo4j db as floats, normally it uploads as string
-def stringsToFloats():
-    query = "MATCH (cp:ClientProfile),(cc:CreditCard),()-[r:FIDELITY]->(),()-[r2:USES_CARD]->() "\
-            "SET cp.age = toFloat(cp.age), cp.dependents = toFloat(cp.dependents), cc.available_limit = toFloat(cc.available_limit), cc.limit = toFloat(cc.limit), cc.number_txs12 = toFloat(cc.number_txs12), cc.number_txs_variation_Q4_Q1 = toFloat(cc.number_txs_variation_Q4_Q1), cc.txs12 = toFloat(cc.txs12), cc.txs_variation_Q4_Q1 = toFloat(cc.txs_variation_Q4_Q1), cc.used_limit = toFloat(cc.used_limit), r.inactivity12 = toFloat(r.inactivity12), r.months_as_client = toFloat(r.months_as_client), r.products_subscribed = toFloat(r.products_subscribed), r.support_requests12 = toFloat(r.support_requests12), r2.utilization_rate = toFloat(r2.utilization_rate) "
+            "WITH toFloat(replace(line.CardUtilizationRate, ',', '.')) as utilization_rate, toFloat(replace(line.Limit, ',', '.')) as limit, toFloat(replace(line.UsedLimit, ',', '.')) as used_limit, toFloat(replace(line.AvailableLimit, ',', '.')) as available_limit, toFloat(replace(line.TransactionsvaluevariationQ4_Q1, ',', '.')) as txs_variation_Q4_Q1, toFloat(replace(line.Transactionsvalue12months, ',', '.')) as txs12, toFloat(replace(line.Transactions12months, ',', '.')) as number_txs12, toFloat(replace(line.VariationTransactionsQ4_Q1, ',', '.')) as number_txs_variation_Q4_Q1, line, cp "\
+            "MERGE (cp)-[r:USES_CARD {utilization_rate: utilization_rate}]->(cc:CreditCard {type: line.CardType, limit: limit, used_limit: used_limit, available_limit: available_limit, txs_variation_Q4_Q1: txs_variation_Q4_Q1, txs12: txs12, number_txs12: number_txs12, number_txs_variation_Q4_Q1: number_txs_variation_Q4_Q1}) "
     graph.run(query)
 
 #calculates a score from Fidelity relation data between client category and profile, adds it to profile node
